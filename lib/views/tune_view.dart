b268926b05cc1ed1cbeb35ffed4c0128f52ed5fa
tharwatsamy <tharwatsamy2004@gmail.com>
2023-07-15 01:36:15 +0300

    create tune view

---
+++ lib/views/tune_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TuneView extends StatelessWidget {
+  const TuneView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
