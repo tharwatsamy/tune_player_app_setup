657b82c6703230e1003828f7ac28bb6b014e1d2c
tharwatsamy <tharwatsamy2004@gmail.com>
2023-07-18 01:36:25 +0300

    display data using loop

--- lib/views/tune_view.dart
+++ lib/views/tune_view.dart
@@ -4,6 +4,17 @@ import 'package:music_notes_player_app_setup/widgets/tune_item.dart';
 class TuneView extends StatelessWidget {
   const TuneView({super.key});
 
+  final List<Color> tuneColors = const [
+    Color(0xffF44336),
+    Color(0xffF89800),
+    Color(0xffFEEB3B),
+    Color(0xff4CAF50),
+    Color(0xff2F9688),
+    Color(0xff2896F3),
+    Color(0xff9C27B0),
+    Color(0xff9C27B0),
+    Color(0xffFEEB3B),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,31 +24,18 @@ class TuneView extends StatelessWidget {
         backgroundColor: const Color(0xff253238),
         elevation: 0,
       ),
-      body: const Column(
-        children: [
-          TuneItem(
-            color: Color(0xffFE403C),
-          ),
-          TuneItem(
-            color: Colors.orange,
-          ),
-          TuneItem(
-            color: Colors.black,
-          ),
-          TuneItem(
-            color: Colors.blue,
-          ),
-          TuneItem(
-            color: Colors.red,
-          ),
-          TuneItem(
-            color: Colors.green,
-          ),
-          TuneItem(
-            color: Colors.yellow,
-          )
-        ],
+      body: Column(
+        children: getTuneItems(),
       ),
     );
+  }
+
+  List<TuneItem> getTuneItems() {
+    List<TuneItem> items = [];
+
+    for (var color in tuneColors) {
+      items.add(TuneItem(color: color));
+    }
+    return items;
   }
 }
